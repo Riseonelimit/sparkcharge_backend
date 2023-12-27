22deec92ef0566897d752db401ad77023e7b6cd8
Alone_X <mandarjoshi079@gmail.com>
2023-12-27 23:21:59 +0530

    Booking Controller

--- src/routes/station.routes.js
+++ src/routes/station.routes.js
@@ -1,7 +1,11 @@
 const{ 
     addStation,
     removeStation,
-    getStationByLocation
+    getStationByLocation,
+    addSlot,
+    removeSlot,
+    getStationSlotByLocation,
+    getSlotByStation
 } = require('../controllers/station.controller.js');
 //Import User Controller from user.controller.js
 
@@ -13,12 +17,20 @@ const Router = require('express').Router;
 const router = Router();
 //Create Router
 
+router.route("/locate-station").get(verifyJWT, getStationByLocation)
+//Locate-Station Secure Route
+router.route("/locate-slot").get(verifyJWT, getStationSlotByLocation)
+//Locate-Slot Secure Route
+router.route("/getslotbystation").get(verifyJWT, getSlotByStation)
+//Locate Slot by Station Secure Route
 router.route("/add-station").post(verifyJWT, addStation)
 //Secure Add Station Route
 router.route("/remove-station").post(verifyJWT, removeStation)
 //Secure Remove Station Route
-router.route("/locate-station").get(verifyJWT, getStationByLocation)
-//Locate-Station Secure Route
+router.route("/add-slot").post(verifyJWT, addSlot)
+//Secure Add Slot Route
+router.route("/remove-slot").post(verifyJWT, removeSlot)
+//Secure Remove Slot Route
 
 module.exports = router;
 //Export User Router
