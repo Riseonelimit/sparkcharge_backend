c5f7b5764deaaf50536c73c61c28d7ec71dd933b
Alone_X <mandarjoshi079@gmail.com>
2024-01-06 19:44:34 +0530

    Major Update v2
    -Reservation Implementation v1.0
    -Removed Semi Colons -_-
    - Reconfigured Cronjob to midnight
    - Added Booking Cleanup in Cronjob

--- src/index.js
+++ src/index.js
@@ -1,27 +1,20 @@
-require('dotenv').config();
+require('dotenv').config()
 //Import Environment Variables from .env file
-const db_conn = require("./db/db_conn.js");
+const db_conn = require("./db/db_conn.js")
 //Import Database Connection Function
-const app = require("./app.js");
+const app = require("./app.js")
 //Import Express App
-const https = require('https');
-const fs = require('fs');
-const options = {
-  key: fs.readFileSync(`${__dirname}/cert/privkey.pem`),
-  cert: fs.readFileSync(`${__dirname}/cert/cert.pem`),
-};
-const server = https.createServer(options, app);
 db_conn()
 //Establish Database Connection
 .then(() => {
-    server.listen(process.env.PORT || 8081, () => {
+    app.listen(process.env.PORT || 8081, () => {
     //Start Express Server
-        console.log(`Server listening at PORT ${process.env.PORT}`);
-    });
+        console.log(`Server listening at PORT ${process.env.PORT}`)
+    })
 })
 .catch((err) => {
 //Catch Database Connection Error
-    console.log("Database Connection Failed!");
-    console.log(err);
-    process.exit(1);
+    console.log("Database Connection Failed!")
+    console.log(err)
+    process.exit(1)
 });
